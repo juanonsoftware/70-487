f287a2db0c6b7c48f2c03f90c00d21ea361110cd
juanonsoftware <juanonsoftware@github.com>
2015-09-29 09:48:38 +0700

    Async/Await

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessRecords();
+            Console.WriteLine("Wait for the result...");
+            Console.ReadLine();
+        }
+
+        static async void ProcessRecords()
+        {
+            var repository = new Repository();
+            var records = await repository.CountRecords();
+            Console.WriteLine("Records: " + records);
+        }
+    }
+}
---
+++ AsyncAwait/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Repository
+    {
+        public async Task<int> CountRecords()
+        {
+            await Task.Factory.StartNew(() => Thread.Sleep(TimeSpan.FromSeconds(20)));
+            return 10;
+        }
+    }
+}
