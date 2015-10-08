a494fbdccf5b64c95e35d91fdb76e33e7556d411
juanonsoftware <juanonsoftware@github.com>
2015-10-08 11:39:17 +0700

    SelfHost is working

---
+++ ChattyApp/ClientToSelfHost/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using ClientToSelfHost.ServiceReference1;
+
+namespace ClientToSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var proxy = new MessageServiceClient();
+                if (proxy.ClientCredentials == null)
+                {
+                    return;
+                }
+
+                proxy.ClientCredentials.UserName.UserName = "hhoangvan";
+                proxy.ClientCredentials.UserName.Password = "hhoangvan";
+
+                var result = proxy.GetAll();
+                Console.WriteLine("Results: " + result.Count());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("-----");
+
+                while (ex != null)
+                {
+                    Console.WriteLine(ex.Message);
+                    ex = ex.InnerException;
+                }
+
+                Console.WriteLine("-----");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
